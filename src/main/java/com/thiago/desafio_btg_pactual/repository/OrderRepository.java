fff575852a7a2fea3fd576ecced2d0c0b0b3eddd
Thiago <thiagopaulista50@gmail.com>
2026-09-07 06:34:13 -0300

    feat: completion of the backend challenge

--- src/main/java/com/thiago/desafio_btg_pactual/repository/OrderRepository.java
+++ src/main/java/com/thiago/desafio_btg_pactual/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.thiago.desafio_btg_pactual.repository;
 
+import com.thiago.desafio_btg_pactual.controller.dto.OrderResponse;
 import com.thiago.desafio_btg_pactual.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends MongoRepository<OrderEntity, Long> {
+    Page<OrderEntity> findAllByCustomerId(Long customerId, PageRequest pageRequest);
 }
--- src/main/java/com/thiago/desafio_btg_pactual/service/OrderService.java
+++ src/main/java/com/thiago/desafio_btg_pactual/service/OrderService.java
@@ -1,21 +1,32 @@
 package com.thiago.desafio_btg_pactual.service;
 
+import com.thiago.desafio_btg_pactual.controller.dto.OrderResponse;
 import com.thiago.desafio_btg_pactual.entity.OrderEntity;
 import com.thiago.desafio_btg_pactual.entity.OrderItem;
 import com.thiago.desafio_btg_pactual.listener.dto.OrderCreatedEvent;
 import com.thiago.desafio_btg_pactual.repository.OrderRepository;
+import org.bson.Document;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository){
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate){
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event){
@@ -28,6 +39,23 @@ public class OrderService {
 
         orderRepository.save(entity);
 
+    }
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest){
+        var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
+
+        return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalByCustomerId(Long customerId){
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
     }
 
     private BigDecimal getTotal(OrderCreatedEvent event) {
